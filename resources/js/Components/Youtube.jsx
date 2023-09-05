5c680bad309c47b1ce0d226f61a98a06bacbb3ab
feryanuar24 <feryanuar24@gmail.com>
2023-09-05 09:46:55 +0700

    fix: youtube API response

--- resources/js/Components/Youtube.jsx
+++ resources/js/Components/Youtube.jsx
@@ -6,39 +6,62 @@ export default function Youtube() {
     const maxResults = 3;
 
     const [videos, setVideos] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}`;
 
         fetch(apiUrl)
-            .then((response) => response.json())
+            .then((response) => {
+                if (response.status === 403) {
+                    setError("API error 403: Akses ditolak");
+                    return null;
+                }
+                return response.json();
+            })
             .then((data) => {
                 setVideos(data.items);
             })
-            .catch((error) => console.error(error));
+            .catch((error) => "Terjadi kesalahan dalam melakukan permintaan");
     }, []);
 
     return (
-        <div className="p-5">
-            <h2 className="font-bold text-3xl border-b-8 w-max border-red-500 uppercase">
-                <a href="https://www.youtube.com/@LPMUNSIKA">LPM Channel</a>
-            </h2>
-            <hr />
-            <div>
-                {videos.map((video) => (
-                    <iframe
-                        key={video.id.videoId}
-                        src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                        title={video.snippet.title}
-                        allowFullScreen
-                        style={{ borderRadius: "12px" }}
-                        width="100%"
-                        height="352"
-                        loading="lazy"
-                        className="mt-10 h-60"
-                    />
-                ))}
-            </div>
+        <div>
+            {error ? (
+                <div className="p-5">
+                    <h2 className="font-bold text-3xl border-b-8 w-max border-red-500 uppercase">
+                        <a href="https://www.youtube.com/@LPMUNSIKA">
+                            LPM Channel
+                        </a>
+                    </h2>
+                    <hr />
+                    <div className="mt-10">{error}</div>
+                </div>
+            ) : (
+                <div className="p-5">
+                    <h2 className="font-bold text-3xl border-b-8 w-max border-red-500 uppercase">
+                        <a href="https://www.youtube.com/@LPMUNSIKA">
+                            LPM Channel
+                        </a>
+                    </h2>
+                    <hr />
+                    <div>
+                        {videos.map((video) => (
+                            <iframe
+                                key={video.id.videoId}
+                                src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                                title={video.snippet.title}
+                                allowFullScreen
+                                style={{ borderRadius: "12px" }}
+                                width="100%"
+                                height="352"
+                                loading="lazy"
+                                className="mt-10 h-60"
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
